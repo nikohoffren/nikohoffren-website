1a96bcff614748783ff261390d677f557f80b11b
Niko Hoffren <niko.hoffren@gmail.com>
2023-03-22 14:27:23 +0200

    fix home screen top margin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import Typing from 'react-typing-effect';
 
 export default function Home() {
     return <>
-        <div className="screen-padding" />
+    <div className="container"></div>
         {/* <img src="NHlogo.jpg" alt="Niko Hoffrén logo" className="NHlogo" /> */}
         <section class="home">
             <div className="bio-info">
                 <div className="header2-hero">
                     <div className="header2-info">
                     <h1><Typing cursorClassName="red-text" speed={200} text={['I\'m Niko Hoffrén']} /></h1>
-                    <h2><Typing cursorClassName="red-text" speed={200} text={['Full stack development student & music producer']} /></h2>
+                    <h2><Typing cursorClassName="red-text" speed={200} text={['Full stack developer & music producer']} /></h2>
 
                         <br />
                         {/* <p class="bold">Welcome to my website! Here you can find my personal<br /> Portfolio and other info</p> */}
