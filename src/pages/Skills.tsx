801f41dc076af60ca7a948cfeda8e389f09ea23b
Niko Hoffren <niko.hoffren@gmail.com>
2023-07-06 19:28:24 +0300

    modify the list items in Skills section

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -5,59 +5,76 @@ export default function Skills() {
     const { language } = useContext(LanguageContext);
 
     return (
-        <section className="py-10 px-8 md:px-16 shadow-lg rounded-lg my-20 mx-4 md:mx-8 lg:mx-16">
+        <section className="flex flex-col items-center justify-center py-10 px-8 md:px-16 shadow-lg rounded-lg my-20 mx-4 md:mx-8 lg:mx-16">
             <h1 className="text-4xl font-bold mb-10 text-center">
                 {language === "en" ? "SKILLS" : "TAIDOT"}
             </h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
-                <div className="p-6 max-w-sm mx-auto rounded-xl shadow-md flex items-center space-x-4 bg-gray-600 text-white">
+                <div className="p-6 max-w-sm mx-auto rounded-xl shadow-md flex items-start space-x-4 bg-white text-gray-900">
                     <div>
-                        <p className="text-lg font-semibold mb-4">
+                        <p className="text-lg font-semibold mb-4 underline">
                             {language === "en"
                                 ? "PROGRAMMING LANGUAGES AND FRAMEWORKS:"
                                 : "OHJELMOINTIKIELET JA RAJAPINNAT:"}
                         </p>
-                        <ul className="list-disc list-inside">
-                            <li>JavaScript</li>
-                            <li>TypeScript</li>
-                            <li>React</li>
-                            <li>PHP</li>
-                            <li>SQL (MySQL, MariaDB)</li>
-                            <li>Java</li>
-                            <li>Dart (Flutter)</li>
-                            <li>Ruby (on Rails)</li>
+                        <ul className="space-y-2">
+                            {[
+                                "JavaScript",
+                                "TypeScript",
+                                "React",
+                                "PHP",
+                                "SQL (MySQL, MariaDB)",
+                                "Java",
+                                "Dart (Flutter)",
+                                "Ruby (on Rails)",
+                            ].map((skill) => (
+                                <li
+                                    key={skill}
+                                    className="py-2 px-4 rounded shadow transition-transform duration-200 transform hover:scale-105 bg-gray-100"
+                                >
+                                    {skill}
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
-                <div className="p-6 max-w-sm mx-auto rounded-xl shadow-md flex items-center space-x-4 bg-gray-600 text-white">
+                <div className="p-6 max-w-sm mx-auto rounded-xl shadow-md flex items-start space-x-4 bg-white text-gray-900">
                     <div>
-                        <p className="text-lg font-semibold mb-4">
+                        <p className="text-lg font-semibold mb-4 underline">
                             {language === "en"
                                 ? "LINKEDIN SKILLS ASSESSMENT BADGES:"
                                 : "LINKEDIN OSAAMISEN ARVIOINTIMERKIT:"}
                         </p>
-                        <ul className="list-disc list-inside">
-                            <li>GNU/Linux</li>
-                            <li>Bash</li>
-                            <li>Node.js</li>
-                            <li>REST APIs</li>
-                            <li>JSON</li>
-                            <li>JavaScript</li>
-                            <li>Cascading Style Sheets (CSS)</li>
-                            <li>React.js</li>
-                            <li>PHP</li>
-                            <li>Ruby on Rails</li>
-                            <li>Scala</li>
-                            <li>Object-Oriented Programming (OOP)</li>
-                            <li>Android</li>
-                            <li>WordPress</li>
-                            <li>Google Cloud Platform (GCP)</li>
-                            <li>Google Analytics</li>
-                            <li>Search Engine Optimization (SEO)</li>
+                        <ul className="space-y-2">
+                            {[
+                                "GNU/Linux",
+                                "Bash",
+                                "Node.js",
+                                "REST APIs",
+                                "JSON",
+                                "JavaScript",
+                                "Cascading Style Sheets (CSS)",
+                                "React.js",
+                                "PHP",
+                                "Ruby on Rails",
+                                "Scala",
+                                "Object-Oriented Programming (OOP)",
+                                "Android",
+                                "WordPress",
+                                "Google Cloud Platform (GCP)",
+                                "Google Analytics",
+                                "Search Engine Optimization (SEO)",
+                            ].map((skill) => (
+                                <li
+                                    key={skill}
+                                    className="py-2 px-4 rounded shadow transition-transform duration-200 transform hover:scale-105 bg-gray-100"
+                                >
+                                    {skill}
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
-
             </div>
         </section>
     );
