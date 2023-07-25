b23838923f48d4ac342792a26ef13e010532723c
Niko Hoffrén <niko.hoffren@gmail.com>
2023-07-25 16:41:49 +0300

    Add new Skill icons to Skills.tsx

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,11 +1,5 @@
 import React, { useContext } from "react";
 import { LanguageContext } from "../common/components/LanguageContext";
-import { skillUrls } from "../common/constants/SkillUrls";
-import { skillIcons } from "../common/constants/SkillIcons";
-import {
-    skillCategories,
-    categoryNames,
-} from "../common/constants/SkillCategories";
 
 export default function Skills() {
     const { language } = useContext(LanguageContext);
@@ -16,41 +10,121 @@ export default function Skills() {
             <h1 className="text-4xl font-bold mb-10 text-center prose-lg prose-purple">
                 {language === "en" ? "SKILLS" : "TAIDOT"}
             </h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {Object.keys(skillCategories).map((category) => (
-                    <div
-                        key={category}
-                        className="p-6 min-h-[20rem] min-w-[22rem] mx-auto rounded-xl shadow-md flex flex-col justify-between bg-gray-100 text-gray-800"
-                    >
-                        <div>
-                            <p className="text-lg font-semibold mb-4">
-                                {categoryNames[language][category]}
-                            </p>
-                            <ul className="space-y-3">
-                                {skillCategories[category].map((skill) => (
-                                    <a
-                                        key={skill}
-                                        href={skillUrls[skill] || "#"}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="text-xl"
-                                    >
-                                        <li className="flex items-center space-x-2 py-2 px-4 rounded-md shadow-sm hover:shadow-md transition duration-300 ease-in-out transform hover:scale-105 bg-white hover:bg-gray-200 cursor-pointer">
-                                            <img
-                                                src={skillIcons[skill]}
-                                                alt={`${skill} icon`}
-                                                className="w-8 h-8"
-                                            />
-                                            <span>{skill}</span>
-                                        </li>
-                                    </a>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                ))}
+            <div className="py-5" />
+            <div className="flex flex-wrap justify-center lg:w-2/4 mx-auto">
+
+                <div className="m-4 text-center">
+                    <img
+                        src="https://techstack-generator.vercel.app/react-icon.svg"
+                        alt="icon"
+                        width="80"
+                        height="80"
+                    />
+                    <br />
+                    React
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://techstack-generator.vercel.app/js-icon.svg"
+                        alt="icon"
+                        width="80"
+                        height="80"
+                    />
+                    <br />
+                    JavaScript
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://techstack-generator.vercel.app/ts-icon.svg"
+                        alt="icon"
+                        width="80"
+                        height="80"
+                    />
+                    <br />
+                    TypeScript
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://skillicons.dev/icons?i=html"
+                        width="80"
+                        height="80"
+                        alt="HTML5"
+                    />
+                    <br />
+                    HTML5
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://skillicons.dev/icons?i=css"
+                        width="80"
+                        height="80"
+                        alt="css"
+                    />
+                    <br />
+                    CSS
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://skillicons.dev/icons?i=tailwind"
+                        width="80"
+                        height="80"
+                        alt="tailwind"
+                    />
+                    <br />
+                    Tailwind
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://skillicons.dev/icons?i=flutter"
+                        width="80"
+                        height="80"
+                        alt="flutter"
+                    />
+                    <br />
+                    Flutter
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://skillicons.dev/icons?i=dart"
+                        width="80"
+                        height="80"
+                        alt="dart"
+                    />
+                    <br />
+                    Dart
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://skillicons.dev/icons?i=nodejs"
+                        width="80"
+                        height="80"
+                        alt="nodejs"
+                    />
+                    <br />
+                    NodeJS
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://skillicons.dev/icons?i=linux"
+                        width="80"
+                        height="80"
+                        alt="linux"
+                    />
+                    <br />
+                    Linux
+                </div>
+                <div className="m-4 text-center">
+                    <img
+                        src="https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png"
+                        width="80"
+                        height="80"
+                        alt="Git"
+                    />
+                    <br />
+                    Git
+                </div>
             </div>
+            <div className="py-10" />
         </section>
     );
 }
-
